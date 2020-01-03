d5f7c4ef33709d69210b8ab1f93420279abf06ba
orsevic <orsevic@gmail.com>
2020-01-03 17:59:03 +0300

    fix auth schema

--- docker/postgresql/docker-entrypoint-initdb.d/init-jsmartbot-db.sql
+++ docker/postgresql/docker-entrypoint-initdb.d/init-jsmartbot-db.sql
@@ -7,6 +7,11 @@ create extension if not exists "hstore" schema pg_catalog;
 
 \connect jsmartbot jsmartbot;
 
+create schema "auth";
+grant all privileges on                  schema "auth" to jsmartbot;
+grant all privileges on all tables    in schema "auth" to jsmartbot;
+grant all privileges on all sequences in schema "auth" to jsmartbot;
+
 create schema "bot";
 grant all privileges on                  schema "bot" to jsmartbot;
 grant all privileges on all tables    in schema "bot" to jsmartbot;
